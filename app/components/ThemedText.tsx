3ac2572a2669e8efaca0de4c620a93384a2f3906
gustavoabel <gustavo.abel@totvs.com.br>
2024-08-27 22:46:48 -0300

    chore: move folders into app and remove unnecessary files

--- app/components/ThemedText.tsx
+++ app/components/ThemedText.tsx
@@ -1,6 +1,6 @@
 import { Text, type TextProps, StyleSheet } from 'react-native';
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from '@/app/hooks/useThemeColor';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
--- app/theme/colors.ts
+++ app/theme/colors.ts
@@ -1,12 +1,7 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
-export const Colors = {
+export const colors = {
   light: {
     text: '#11181C',
     background: '#fff',
